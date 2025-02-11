9f4147ed1b151009637bc0b34d4b41b11b4f8663
juangbagusaryamukti <juang45oke@gmail.com>
2025-02-11 09:02:21 +0700

    AUTHENTICATE USER MENGGUNAKAN JWT TOKEN LARAVEL

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,8 +7,9 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use PHPOpenSourceSaver\JWTAuth\Contracts\JWTSubject;
 
-class User extends Authenticatable
+class User extends Authenticatable implements JWTSubject
 {
     use HasApiTokens, HasFactory, Notifiable;
 
@@ -25,6 +26,19 @@ class User extends Authenticatable
         "role",
         'password',
     ];
+
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    public function getJWTCustomClaims()
+    {
+        return [
+            'email'=>$this->email,
+            'name'=>$this->name
+        ];
+    }
 
     /**
      * The attributes that should be hidden for serialization.
--- routes/api.php
+++ routes/api.php
@@ -17,23 +17,24 @@ use App\Models\Movie;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
-Route::post("register_admin",[AuthController::class,"register"]);
-Route::get("/get_user",[AuthController::class,"getUser"]);
-Route::get("/get_detail_user/{id}",[AuthController::class,"getDetailUser"]);
-Route::put("/update_user/{id}",[AuthController::class,"update_user"]);
-Route::delete("/hapus_user/{id}",[AuthController::class,"hapus_user"]);
 
-Route::post("register_category",[CategoryController::class,"register"]);
-Route::get("/get_category",[CategoryController::class,"getCategory"]);
-Route::get("/get_detail_category/{id}",[CategoryController::class,"getDetailCategory"]); 
-Route::put("/update_category/{id}",[CategoryController::class,"update_Category"]);
-Route::delete("/hapus_category/{id}",[CategoryController::class,"hapus_Category"]);
+Route::post("register_admin", [AuthController::class, "register"]);
+Route::get("/get_user", [AuthController::class, "getUser"]);
+Route::get("/get_detail_user/{id}", [AuthController::class, "getDetailUser"]);
+Route::put("/update_user/{id}", [AuthController::class, "update_user"]);
+Route::delete("/hapus_user/{id}", [AuthController::class, "hapus_user"]);
 
-Route::post("register_movie",[MovieController::class,"register"]);
-Route::get("/get_movie",[MovieController::class,"getMovie"]);
-Route::get("/get_detail_movie/{id}",[MovieController::class,"getDetailMovie"]); 
-Route::put("/update_movie/{id}",[MovieController::class,"update_Movie"]);
-Route::delete("/hapus_movie/{id}",[MovieController::class,"hapus_Movie"]);
+Route::post("register_category", [CategoryController::class, "register"]);
+Route::get("/get_category", [CategoryController::class, "getCategory"]);
+Route::get("/get_detail_category/{id}", [CategoryController::class, "getDetailCategory"]);
+Route::put("/update_category/{id}", [CategoryController::class, "update_Category"]);
+Route::delete("/hapus_category/{id}", [CategoryController::class, "hapus_Category"]);
+
+Route::post("register_movie", [MovieController::class, "register"]);
+Route::get("/get_movie", [MovieController::class, "getMovie"]);
+Route::get("/get_detail_movie/{id}", [MovieController::class, "getDetailMovie"]);
+Route::put("/update_movie/{id}", [MovieController::class, "update_Movie"]);
+Route::delete("/hapus_movie/{id}", [MovieController::class, "hapus_Movie"]);
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
@@ -43,3 +44,6 @@ Route::middleware('auth:sanctum')->get('/category', function (Request $request) 
 Route::middleware('auth:sanctum')->get('/movie', function (Request $request) {
     return $request->Movie();
 });
+
+Route::post("/login", [AuthController::class, "login"]);
+Route::get("/logout", [AuthController::class, "logout"]);
